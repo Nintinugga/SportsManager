470bbd08df1c7e23d400580376d3567368ae91dc
Nintinugga <Jonas_Huber2@gmx.de>
2017-05-08 17:45:50 +0200

    Finished MainView init commit of EventsView

--- app/src/main/java/com/comp/ninti/sportsmanager/MainActivity.java
+++ app/src/main/java/com/comp/ninti/sportsmanager/MainActivity.java
@@ -1,7 +1,9 @@
 package com.comp.ninti.sportsmanager;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.customers) {
+            System.out.println("Costumers Clicked");
+        } else if (v.getId() == R.id.disciplines) {
+            System.out.println("disciplines Clicked");
+        } else if (v.getId() == R.id.rules) {
+            System.out.println("rules Clicked");
+        } else if (v.getId() == R.id.events) {
+            System.out.println("events Clicked");
+        }
     }
 }

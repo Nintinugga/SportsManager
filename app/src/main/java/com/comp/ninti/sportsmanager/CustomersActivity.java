79a41d23d72819ffbad24e732629bdbf3d4397bb
Nintinugga <Jonas_Huber2@gmx.de>
2017-05-18 17:10:24 +0200

    added ic_customer.xml customer icon
    - changed activity_customers.xml fab with image button which holds the ic_customer.xml
    - activity_main.xml uses ic_customer.xml
    - added colors.xml "theGreen" which is the green of appIcon and ic_customer.xml background
    - changed height of buttons in activity_main.xml
    - added new String to strings.xml which describes the imagebuttons content

--- app/src/main/java/com/comp/ninti/sportsmanager/CustomersActivity.java
+++ app/src/main/java/com/comp/ninti/sportsmanager/CustomersActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class CustomersActivity extends AppCompatActivity {
 
@@ -16,8 +17,8 @@ public class CustomersActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        ImageButton addNewCustomer = (ImageButton) findViewById(R.id.addNewCustomer);
+        addNewCustomer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent myIntent = new Intent(CustomersActivity.this, CustomerDetail.class);

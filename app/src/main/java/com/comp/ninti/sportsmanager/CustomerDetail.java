58f6ebac8cdc4d9cd11f739364bf3cbc0f4c5ad5
Nintinugga <Jonas_Huber2@gmx.de>
2017-05-18 15:32:03 +0200

    refactored CustomerDetail.java to be a coordinator layout
    - added spinner to content_customer_detail.xml for age

--- app/src/main/java/com/comp/ninti/sportsmanager/CustomerDetail.java
+++ app/src/main/java/com/comp/ninti/sportsmanager/CustomerDetail.java
@@ -1,27 +1,45 @@
 package com.comp.ninti.sportsmanager;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.comp.ninti.database.CustomerContract;
 import com.comp.ninti.database.DbHandler;
 import com.comp.ninti.general.Customer;
 
+import java.util.ArrayList;
+
 public class CustomerDetail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_customer_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ArrayList<Integer> age = new ArrayList<Integer>();
+        for (int i = 1; i <= 100; i++) {
+            age.add(i);
+        }
+        ArrayAdapter<Integer> spinnerArrayAdapter = new ArrayAdapter<Integer>(
+                this, android.R.layout.simple_spinner_item, age);
+        spinnerArrayAdapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
+
+        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+        spinner.setAdapter(spinnerArrayAdapter);
         Button addCustomerBtn = (Button) findViewById(R.id.addCustomer);
         addCustomerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String name = ((EditText) findViewById(R.id.name)).getText().toString();
+                String name = ((EditText) findViewById(R.id.customerName)).getText().toString();
                 if (name == null || name.isEmpty() || name.equals(R.string.Name)) {
                     Toast.makeText(CustomerDetail.this, "Use an actual Name!",
                             Toast.LENGTH_LONG).show();
@@ -41,5 +59,7 @@ public class CustomerDetail extends AppCompatActivity {
                 }
             }
         });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
 }

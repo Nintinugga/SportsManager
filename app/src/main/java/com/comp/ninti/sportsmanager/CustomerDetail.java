2fb023c45e25fc3173451f08599a82d93ee43cf1
Nintinugga <jonas_huber2@gmx.de>
2017-05-23 12:17:12 +0200

    CustomerDetail finished, changed activity_customers.xml
    - fixed navigation through all inputs in CustomerDetail.java
    - CustomerDetail.java adds the customer now with name, email, age and telephone if provided
    - added fab dimensions to dimens.xml
    - added bottom add button dimension to dimens.xml
    - age is now the last input in content_customer_detail.xml

--- app/src/main/java/com/comp/ninti/sportsmanager/CustomerDetail.java
+++ app/src/main/java/com/comp/ninti/sportsmanager/CustomerDetail.java
@@ -1,13 +1,18 @@
 package com.comp.ninti.sportsmanager;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.comp.ninti.database.CustomerContract;
@@ -31,11 +36,14 @@ public class CustomerDetail extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String name = ((EditText) findViewById(R.id.customerName)).getText().toString();
+                String email = ((EditText) findViewById(R.id.customerName)).getText().toString();
+                String phone = ((EditText) findViewById(R.id.customerName)).getText().toString();
+                int age = Integer.valueOf(((Spinner) findViewById(R.id.customerAge)).getSelectedItem().toString());
                 if (name == null || name.isEmpty() || name.equals(R.string.Name)) {
                     Toast.makeText(CustomerDetail.this, "Use an actual Name!",
                             Toast.LENGTH_LONG).show();
                 } else {
-                    Customer customer = new Customer(name);
+                    Customer customer = new Customer(name, age, email, phone);
                     DbHandler dbHandler = new DbHandler(CustomerDetail.this, "", null, 1);
                     long returnVal;
                     if ((returnVal = dbHandler.getWritableDatabase().insert(CustomerContract.CUSTOMER.TABLE_NAME, null, CustomerContract.getInsert(customer))) == -1) {
@@ -46,11 +54,30 @@ public class CustomerDetail extends AppCompatActivity {
                                 Toast.LENGTH_LONG).show();
                     }
                     dbHandler.close();
-
+                    finish();
                 }
             }
         });
+        EditText editText = (EditText) findViewById(R.id.customerEmail);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_NEXT) {
+                    hideKeyboard();
+                    v.clearFocus();
+                    Spinner spinner = (Spinner) findViewById(R.id.customerAge);
+                    spinner.requestFocus();
+                    spinner.performClick();
+                    handled = true;
+                }
+                return handled;
+            }
+        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        EditText editText1 = (EditText) findViewById(R.id.customerName);
+        editText1.requestFocus();
     }
 
     private void createSpinnerForAge() {
@@ -62,8 +89,14 @@ public class CustomerDetail extends AppCompatActivity {
                 this, android.R.layout.simple_spinner_item, age);
         spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        Spinner spinner = (Spinner) findViewById(R.id.customerAge);
         spinner.setAdapter(spinnerArrayAdapter);
+    }
+
+    private void hideKeyboard() {
+        InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(),
+                InputMethodManager.HIDE_NOT_ALWAYS);
     }
 
 }

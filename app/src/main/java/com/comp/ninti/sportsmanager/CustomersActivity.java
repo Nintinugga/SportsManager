54c9eed524b269416bf83a89e6a9b1b39f703472
Nintinugga <jonas_huber2@gmx.de>
2017-05-23 14:06:12 +0200

    added toast to CustomersActivity.java which displayes name and id of clicked item

--- app/src/main/java/com/comp/ninti/sportsmanager/CustomersActivity.java
+++ app/src/main/java/com/comp/ninti/sportsmanager/CustomersActivity.java
@@ -1,19 +1,24 @@
 package com.comp.ninti.sportsmanager;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.comp.ninti.database.CustomerContract;
 import com.comp.ninti.database.DbHandler;
+import com.comp.ninti.general.Customer;
 
 public class CustomersActivity extends AppCompatActivity {
     private DbHandler dbHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,15 +37,29 @@ public class CustomersActivity extends AppCompatActivity {
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         displayItems();
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Cursor c = (Cursor) parent.getAdapter().getItem(position);
+                c.moveToPosition(position);
+                Customer clickedCustomer = new Customer(c.getString(c.getColumnIndex(CustomerContract.CUSTOMER.COLUMN_NAME)),
+                        c.getInt(c.getColumnIndex(CustomerContract.CUSTOMER.COLUMN_AGE)), c.getString(c.getColumnIndex(CustomerContract.CUSTOMER.COLUMN_EMAIL)),
+                        c.getString(c.getColumnIndex(CustomerContract.CUSTOMER.COLUMN_PHONE)));
+                Toast.makeText(CustomersActivity.this, "Clicked Customer: " + clickedCustomer.getName() + " dbID: " + id,
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
-    private void displayItems(){
+    private void displayItems() {
         dbHandler = new DbHandler(CustomersActivity.this, "", null, 1);
         SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
                 android.R.layout.simple_list_item_2,
                 dbHandler.getAllCustomers(),
-                new String[] {CustomerContract.CUSTOMER.COLUMN_NAME, CustomerContract.CUSTOMER.COLUMN_EMAIL},
-                new int[] { android.R.id.text1, android.R.id.text2 });
+                new String[]{CustomerContract.CUSTOMER.COLUMN_NAME, CustomerContract.CUSTOMER.COLUMN_EMAIL},
+                new int[]{android.R.id.text1, android.R.id.text2});
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(adapter);
     }
@@ -48,7 +67,7 @@ public class CustomersActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(dbHandler != null)
+        if (dbHandler != null)
             dbHandler.close();
     }
 

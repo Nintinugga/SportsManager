6861b628bfd39449f8d1f7edd8ed9298ea7ed84c
Nintinugga <jonas_huber2@gmx.de>
2017-05-23 14:52:20 +0200

    Added RuleDetail.java and Activity, Added start of RuleDetail.java on fab in RulesActivity.java

--- app/src/main/java/com/comp/ninti/sportsmanager/RuleDetail.java
+++ app/src/main/java/com/comp/ninti/sportsmanager/RuleDetail.java
@@ -1,31 +1,25 @@
 package com.comp.ninti.sportsmanager;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
-public class RulesActivity extends AppCompatActivity {
+public class RuleDetail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rules);
+        setContentView(R.layout.activity_rule_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent myIntent = new Intent(RulesActivity.this, RuleDetail.class);
-                RulesActivity.this.startActivity(myIntent);
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        EditText editText1 = (EditText) findViewById(R.id.ruleName);
+        editText1.requestFocus();
     }
 
 }

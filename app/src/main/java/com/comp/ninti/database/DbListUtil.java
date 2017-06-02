90195c5b8fefa5c77ce24135184c267e0084c736
Nintinugga <jonas_huber2@gmx.de>
2017-06-02 13:36:06 +0200

    Added DbListUtil.java which helps converting comma seperated strings to string lists or the other way around
    - is needed for some database columns which store lists

---
+++ app/src/main/java/com/comp/ninti/database/DbListUtil.java
@@ -0,0 +1,26 @@
+package com.comp.ninti.database;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DbListUtil {
+    private static String LIST_SEPARATOR = ",";
+
+    public static String convertListToString(List<String> stringList) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String str : stringList) {
+            stringBuilder.append(str).append(LIST_SEPARATOR);
+        }
+
+        // Remove last separator
+        int lastIndex = stringBuilder.lastIndexOf(LIST_SEPARATOR);
+        stringBuilder.delete(lastIndex, lastIndex + LIST_SEPARATOR.length() + 1);
+
+        return stringBuilder.toString();
+    }
+
+    public static List<String> convertStringToList(String str) {
+        return Arrays.asList(str.split(LIST_SEPARATOR));
+    }
+}

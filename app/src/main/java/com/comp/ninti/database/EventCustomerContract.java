25449edfbb84eeaf391004a65f9341cd2cb4181d
Nintinugga <jonas_huber2@gmx.de>
2017-06-09 14:04:34 +0200

    Added EventCustomerContract.java and EventCustomerEntry.java

---
+++ app/src/main/java/com/comp/ninti/database/EventCustomerContract.java
@@ -0,0 +1,39 @@
+package com.comp.ninti.database;
+
+import android.content.ContentValues;
+import android.provider.BaseColumns;
+
+import com.comp.ninti.general.core.EventCustomerEntry;
+
+public class EventCustomerContract {
+    private EventCustomerContract(){
+    }
+    public static final class EVENTCUSTOMER implements BaseColumns{
+        public static final String TABLE_NAME = "eventcustomer";
+        public static final String COLUMN_EV_ID = "eventId";
+        public static final String COLUMN_CU_ID = "custId";
+        public static final String COLUMN_DI_ID = "discId";
+        public static final String COLUMN_ATTEMPT = "attempt";
+        public static final String COLUMN_POINTS ="points";
+    }
+
+    public static final String CREATE_TABLE = "CREATE TABLE " + EVENTCUSTOMER.TABLE_NAME + "("
+            + EVENTCUSTOMER._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + EVENTCUSTOMER.COLUMN_EV_ID + " LONG, "
+            + EVENTCUSTOMER.COLUMN_CU_ID + " LONG, "
+            + EVENTCUSTOMER.COLUMN_DI_ID + " LONG, "
+            + EVENTCUSTOMER.COLUMN_ATTEMPT + " INT, "
+            + EVENTCUSTOMER.COLUMN_POINTS +" INT)";
+
+    public static final String DROP_TABLE = "DROP TABLE IF EXISTS " + EVENTCUSTOMER.TABLE_NAME;
+
+    public static ContentValues getInsert(EventCustomerEntry eventCustomerEntry) {
+        ContentValues values = new ContentValues();
+        values.put(EVENTCUSTOMER.COLUMN_EV_ID, eventCustomerEntry.getEvId());
+        values.put(EVENTCUSTOMER.COLUMN_CU_ID, eventCustomerEntry.getCustId());
+        values.put(EVENTCUSTOMER.COLUMN_DI_ID, eventCustomerEntry.getDiscId());
+        values.put(EVENTCUSTOMER.COLUMN_ATTEMPT, eventCustomerEntry.getAttempt());
+        values.put(EVENTCUSTOMER.COLUMN_POINTS, eventCustomerEntry.getPoints());
+        return values;
+    }
+}
---
+++ app/src/main/java/com/comp/ninti/general/core/EventCustomerEntry.java
@@ -0,0 +1,59 @@
+package com.comp.ninti.general.core;
+
+
+
+public class EventCustomerEntry {
+    private long evId;
+    private long custId;
+    private long discId;
+    private int attempt;
+    private int points;
+
+    public EventCustomerEntry(long evId, long custId, long discId, int attempt, int points) {
+        this.evId = evId;
+        this.custId = custId;
+        this.discId = discId;
+        this.attempt = attempt;
+        this.points = points;
+    }
+
+    public long getEvId() {
+        return evId;
+    }
+
+    public void setEvId(long evId) {
+        this.evId = evId;
+    }
+
+    public long getCustId() {
+        return custId;
+    }
+
+    public void setCustId(long custId) {
+        this.custId = custId;
+    }
+
+    public long getDiscId() {
+        return discId;
+    }
+
+    public void setDiscId(long discId) {
+        this.discId = discId;
+    }
+
+    public int getAttempt() {
+        return attempt;
+    }
+
+    public void setAttempt(int attempt) {
+        this.attempt = attempt;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}
